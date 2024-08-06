fbe999a06b468bd27ece98d54d4fb77acf4bb97b
romazan26 <130604263+romazan26@users.noreply.github.com>
2024-08-06 14:36:54 +0300

    Initial Commit

---
+++ TestStore/TestStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestStoreApp.swift
+//  TestStore
+//
+//  Created by Роман on 06.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct TestStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
